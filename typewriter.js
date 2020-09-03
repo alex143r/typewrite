e94f83d4174a7efbf2240eab8d230a9841d5ebdb
Alexander Tyllesen <alex143r@stud.kea.dk>
2020-09-03 06:08:32 +0200

    added sounds js

--- typewriter.js
+++ typewriter.js
@@ -3,24 +3,50 @@
 let index = 0;
 const word = document.querySelector(".typewritten").innerHTML;
 let letter = word.substring(index, index + 1);
+const audio1 = new Audio("typekey1.mp3")
+const audioSpace = new Audio("typespace.mp3")
 
 console.log(word);
 showText();
+
+let sounds = false;
+
 
 
 function showText() {
     if (index === 0) {
         document.querySelector(".typewritten").innerHTML = letter;
         console.log("this one" + letter);
+        audio1.play();
+
+        document.querySelector("button").addEventListener("click", turnOnSounds);
+
+        function turnOnSounds() {
+            sounds = true;
+        }
         index++;
-        setTimeout(showText, 200);
+        setTimeout(showText, 500);
 
     } else if (word.length > index) {
         let letter = word.substring(index, index + 1);
         console.log(letter);
         document.querySelector(".typewritten").innerHTML += letter;
         index++;
-        setTimeout(showText, 200);
+        document.querySelector("button").addEventListener("click", turnOnSounds);
+
+        function turnOnSounds() {
+            sounds = true;
+        }
+        if (sounds == true) {
+            if (letter === " ") {
+                audioSpace.play();
+            } else {
+                audio1.play();
+
+            }
+        }
+
+        setTimeout(showText, 400 + Math.floor(Math.random() * 400));
     }
 
 }
